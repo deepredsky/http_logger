52911f5079d73291ad94b6ad856d71f717ebe72e
Rajesh Sharma <rajesh@unity3d.com>
2016-06-25 13:34:40 +0200

    Send all routes to react app

--- web/router.ex
+++ web/router.ex
@@ -16,7 +16,7 @@ defmodule HttpLogger.Router do
   scope "/", HttpLogger do
     pipe_through :browser # Use the default browser stack
 
-    get "/", PageController, :index
+    get "*path", PageController, :index
   end
 
   # Other scopes may use custom stacks.
