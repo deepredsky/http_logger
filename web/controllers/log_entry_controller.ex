04fc98e2bb67a620452dc250b036d345f9b8855f
Rajesh Sharma <rajesh@unity3d.com>
2016-06-25 13:34:40 +0200

    fix argument error

--- web/controllers/log_entry_controller.ex
+++ web/controllers/log_entry_controller.ex
@@ -2,7 +2,7 @@ defmodule HttpLogger.LogEntryController do
   use HttpLogger.Web, :controller
 
   def index(conn, _params) do
-    render conn, "index.json", LogEntry.all
+    render conn, "index.json", log_entries: LogEntry.all
   end
 
   def delete_all(conn, _params) do
